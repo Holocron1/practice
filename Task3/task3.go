7bb1e6eb0521f9caea125ded2a2a1a33b1f45731
Матвей <sudarevmatvej@gmail.com>
2026-08-09 08:34:47 +0300

    task3

--- Task3/task3.go
+++ Task3/task3.go
@@ -1,1 +1,25 @@
-package Task3
+package main
+
+import "fmt"
+
+// Person содержит имя и возраст человека.
+type Person struct {
+	Name string
+	Age  int
+}
+
+// TODO: реализуйте Stringer для Person
+
+func main() {
+	alice := Person{Name: "Alice", Age: 30}
+	fmt.Println(alice)
+	// Ожидаемый вывод: Alice (30)
+}
+
+func (p Person) String() string {
+	return fmt.Sprintf("%s (%d)", p.Name, p.Age) //пришлось узнать как работает Sprintf
+}
+
+type Stringer interface {
+	String() string
+}
